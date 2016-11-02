ef8b299cc5e72a3500bd755fbfb656158b652250
arthur-mosnin <arthur.mosnin@gmail.com>
2016-11-02 20:25:15 +0200

    the first step to create a app

---
+++ lab3/calculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Katren on 16.10.2016.
+//  Copyright © 2016 Artur. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
